10fd9dbeea3bdb850e024cea6adf79db26669f6e
Muhammad Ali <muhammadalinaqvi@hotmail.com>
2023-04-10 12:25:59 -0400

    Changed input values

--- TempConv.java
+++ TempConv.java
@@ -1,14 +1,14 @@
 class Demo {
     public static void main(String[] args) {
         //Celsius to Fahrenheit
-        int c = 102;
+        double c = 102;
         double x = (double)9/(double)5;
         int y = 32;
 
         System.out.println((c*x+y) + "°C");
 
         //Fahrenheit to Celsius
-        int f = 102;
+        double f = 215.6;
         double w = (double)5/(double)9;
         int v = 32;
 

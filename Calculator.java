ebab1e5c3aa5953d4565bd0c74ab6735498bdfd6
Muhammad Ali <muhammadalinaqvi@hotmail.com>
2023-08-10 15:37:22 -0400

    Ask for input

--- Calculator.java
+++ Calculator.java
@@ -5,43 +5,46 @@ class Calculate {
     Scanner sc = new Scanner(System.in);
 
     String optr = new String();
+
+    //Ask for operator
+    System.out.println("Choose an operator: +, -, *, or /");
     optr = sc.nextLine();
+
+    //Ask for first number
+    System.out.println("Enter first number");
+    double num1 = sc.nextDouble();
+
+    //Ask for second number
+    System.out.println("Enter second number");
+    double num2 = sc.nextDouble();
 
     //Addition
     if(optr.equals("+")) {
-      double num1 = sc.nextDouble();
-      double num2 = sc.nextDouble();
 
       double sum = (double)num1 + (double)num2;
 
-      System.out.println("The sum of " + num1 + " + " + num2 + " is " + sum);
+      System.out.println(num1 + " + " + num2 + " = " + sum);
 }
     //Subtraction
     if(optr.equals("-")) {
-      double num1 = sc.nextDouble();
-      double num2 = sc.nextDouble();
 
       double diff = (double)num1 - (double)num2;
 
-      System.out.println("The difference of " + num1 + " - " + num2 + " is " + diff);
+      System.out.println(num1 + " - " + num2 + " = " + diff);
 }
     //Multiplication
     if(optr.equals("*")) {
-      double num1 = sc.nextDouble();
-      double num2 = sc.nextDouble();
 
       double prod = (double)num1 * (double)num2;
 
-      System.out.println("The product of " + num1 + " x " + num2 + " is " + prod);
+      System.out.println(num1 + " x " + num2 + " = " + prod);
 }
     //Division
     if(optr.equals("/")) {
-      double num1 = sc.nextDouble();
-      double num2 = sc.nextDouble();
 
       double quot = (double)num1 / (double)num2;
 
-      System.out.println("The quotient of " + num1 + " / " + num2 + " is " + quot);
+      System.out.println(num1 + " / " + num2 + " = " + quot);
 }
     sc.close();
 }
